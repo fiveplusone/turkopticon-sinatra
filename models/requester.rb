c85f20469305ccd0568b2bbda2a054e14bbafdae
M. Six Silberman <six@wtf.tw>
2011-08-18 17:30:35 -0700

    visualizing attributes on requester list

--- models/requester.rb
+++ models/requester.rb
@@ -20,4 +20,22 @@ class Requester < ActiveRecord::Base
     2.5
   end
 
+  def self.visualize(val)
+    vmax = 5.0
+    chars = 30
+    str = "<span class='meter'><span class='meter_full'>"
+    full_fraction = val / vmax
+    full_chars = full_fraction * chars
+    full_chars = full_chars.round
+    full_chars.times do
+      str.concat("&nbsp;")
+    end
+    str.concat("</span><span class='meter_empty'>")
+    empty_chars = chars - full_chars
+    empty_chars.times do
+      str.concat("&nbsp;")
+    end
+    str.concat("</span></span>")
+  end
+
 end
